31a92a8b8cc71a7f2f9b95441a8d802fccd3377d
Shaokee Moua <m.shaokee@gmail.com>
2020-05-20 16:48:20 -0500

    secret displays correctly for specific user

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -2,13 +2,31 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 // ADD REQUIRE HERE
-const { rejectUnauthenticated } = require ('../modules/authentication-middleware');
+// const { rejectUnauthenticated } = require ('../modules/authentication-middleware');
+const { rejectAuthorization } = require('../modules/authorization-middleware');
 
-router.get('/', rejectUnauthenticated, (req, res) => { /* ADD rejectUnauthenticated HERE */
+// router.get('/', rejectUnauthenticated, (req, res) => { /* ADD rejectUnauthenticated HERE */
+//     console.log('req.user:', req.user);
+//     pool.query('SELECT * FROM "secret";')
+//         .then(results => res.send(results.rows))
+//         .catch(error => {
+//             console.log('Error making SELECT for secrets:', error);
+//             res.sendStatus(500);
+//         });
+// });
+
+router.get('/', rejectAuthorization, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
+    // pool.query('SELECT * FROM "user";')
+    let queryString = `SELECT "content" FROM "secret"
+                JOIN "user" ON "user".clearance_level >= "secret".secrecy_level
+                WHERE "user".id = $1;`;
+    console.log(req.user.clearance_level);
+    pool.query(queryString, [req.user.id])
+        .then(results => {
+            console.log(results.rows)
+            res.send(results.rows)
+        }).catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
